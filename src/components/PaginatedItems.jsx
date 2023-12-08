c1a1a7c2b809edd334484fef417eca285de2589a
Kaung Htet San <kghtetsan26@gmail.com>
2023-12-09 00:08:01 +0630

    added ui fixed in pagination

--- src/components/PaginatedItems.jsx
+++ src/components/PaginatedItems.jsx
@@ -7,15 +7,23 @@ export const PaginatedItems = ({ totalPage, handlePageChange }) => {
 
     return (
         <div className="mt-5">
-            <ReactPaginate className="flex flex-row gap-4 p-3"
+            <ReactPaginate
+                className="flex flex-row gap-4 p-3"
                 breakLabel="..."
-                nextLabel="next >"
+                nextLabel="Next"
                 onPageChange={handlePageChange}
                 pageRangeDisplayed={5}
                 pageCount={totalPage}
-                previousLabel="< previous"
+                previousLabel="Previous"
                 renderOnZeroPageCount={null}
+                containerClassName="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                pageLinkClassName="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                previousLinkClassName="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                nextLinkClassName="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                activeClassName=""
             />
+            {/** To test active class name */}
         </div>
+
     );
 }
